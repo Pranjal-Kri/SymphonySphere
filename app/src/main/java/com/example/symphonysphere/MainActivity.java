697dab6758c02c875b14b11af17d0be2d1b0f6b2
Pranjal Krishna Gupta <guptapranjalkrishna07@gmail.com>
2024-07-12 20:28:44 +0530

    First commit

---
+++ app/src/main/java/com/example/symphonysphere/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.symphonysphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Toast.makeText(MainActivity.this, "Runtime Permission given", Toast.LENGTH_SHORT).show();
+                        ArrayList<File> mySongs = fetchsongs(Environment.getExternalStorageDirectory());
+                        String[] items = new String[mySongs.size()];
+                        for (int i = 0; i < mySongs.size(); i++) {
+                            items[i] = mySongs.get(i).getName().replace(".mp3", "");
+                        }
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, items);
+                        listView.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+
+    }
+
+    public ArrayList<File> fetchsongs(File file) {
+        ArrayList arrayList = new ArrayList();
+        File[] songs = file.listFiles();
+        if (songs != null) {
+            for (File myfile : songs) {
+                if (!myfile.isHidden() && myfile.isDirectory()) {
+                    arrayList.addAll(fetchsongs(myfile));
+                } else {
+                    if (myfile.getName().endsWith(".mp3") && !myfile.getName().startsWith(".")) {
+                        arrayList.add(myfile);
+                    }
+                }
+            }
+        }
+
+        return arrayList;
+    }
+}
